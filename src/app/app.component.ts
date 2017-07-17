9cd37f9a769654cbc75d5180b9dfd2b1430f44de
davidblochQ4U <davidbloch03@gmail.com>
2017-07-17 21:35:52 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:` 
+  <h1 [ngStyle]="titleStyles">Hello!</h1>
+  <h2>{{someProperty}}</h2>
+<p [@myAwesomeAnimation]='state' (click)="animateMe()">I will animate</p>
+    `,
+  styles: [`
+h1 {
+	text-decoration:underline;
+}
+
+.red-title{
+	color:red;
+}
+
+.large-title {
+	font-size:4em;
+}
+
+p {
+  width:200px;
+    background:lightgray;
+    margin: 100px auto;
+    text-align:center;
+    padding:20px;
+    font-size:1.5em;
+}
+  `],
+  animations: [
+    trigger('myAwesomeAnimation', [
+        state('small', style({
+            transform: 'scale(1)',
+        })),
+        state('large', style({
+            transform: 'scale(1.2)',
+        })),
+        transition('small <=> large', animate('500ms ease-in', keyframes([
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+          style({opacity: 1, transform: 'translateY(35px)',  offset: 0.5}),
+          style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
+        ]))),
+    ]),
+  ]
+
 })
 export class AppComponent {
-  title = 'app';
+
+state: string = 'small'
+  
+constructor(private dataService:DataService) {
+
+
+}
+
+someProperty:string ='';
+
+ngOnInit(){
+	console.log(this.dataService.cars);
+	this.someProperty = this.dataService.myData();
+}
+
+titleStyles = {
+'color': 'red',
+'font-size' : '4em'
+ }
+
+ animateMe() {
+   this.state = (this.state === 'small' ? 'large': 'small')
+ }
+
 }
